f2ee0eff6d9c3275ed2f25d66b48c123781c7972
gemtunde <gemtunde@gmail.com>
2023-09-28 04:50:02 +0100

    completed api

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,45 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const errorHandler = require("./middleware/errorMiddleware");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 
 //middlewares
-app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
+
+//image upload
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //db connection
 const dbConfig = require("./config/dbConfig");
 
 //Routes
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
+
+//user
+app.use("/api/users/", userRoute);
+
+//product
+app.use("/api/products/", productRoute);
+
+//contact-us
+app.use("/api/contacts/", contactRoute);
+
 app.get("/", (req, res) => {
   res.send({ message: "home details" });
 });
+
+//error handler
+app.use(errorHandler);
 
 //port
 const PORT = process.env.PORT || 5000;
